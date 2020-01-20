121f3028ce885253b61791a68d292465a31b2970
Azriel Policarpio <52266762+Ikusey@users.noreply.github.com>
2020-01-19 20:17:11 -0800

    re uploads assingment 2
    fixes count and int i so the correct numbers for each count are shown

--- Assignment2.java
+++ Assignment2.java
@@ -2,9 +2,9 @@
 public class Assignment2 {
 
 	public static void main(String[] args) {
-		int n1=0,n2=1,count=10;   
+		int n1=0,n2=1,count=13;   
 		
-		 for(int i=2;i<count;++i)
+		 for(int i=0;i<count;++i)
 		 {    
 		  int n3=n1+n2;    
 		  System.out.println(n3);    
